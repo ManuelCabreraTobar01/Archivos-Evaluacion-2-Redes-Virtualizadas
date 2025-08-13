56d122270703c4615f80f55f0fab42d9353cd6d6
Manuel Cabrera Tobar <Manuel.Cabrera@etudiane.ipss.cl>
2025-08-13 02:04:14 +0000

    Docker y CI/CD

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h2>Hola — Sample App en puerto 8888</h2>"
+    return "<h2>Evaluacion2 — Sample App en puerto 8888</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888)
